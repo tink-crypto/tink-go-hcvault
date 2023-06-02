124ddcd72bc991fe58cdc731723558fe89ba0534
Charles Lee <ckl@google.com>
2023-06-02 01:24:52 -0700

    Enable verbose output for Go tests run via Bazel.
    PiperOrigin-RevId: 537247413
    Change-Id: I69f51389ccd18eca600e0ed4763072852c75185e

--- kokoro/macos_external/bazel/run_tests.sh
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -60,6 +60,7 @@ if [[ -n "${KOKORO_ROOT:-}" ]]; then
 fi
 readonly MANUAL_TARGETS
 
-./kokoro/testutils/run_bazel_tests.sh . "${MANUAL_TARGETS[@]}"
+./kokoro/testutils/run_bazel_tests.sh \
+  -t --test_arg=--test.v . "${MANUAL_TARGETS[@]}"
 
 mv WORKSPACE.bak WORKSPACE
