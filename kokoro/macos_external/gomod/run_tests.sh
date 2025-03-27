f1722924e3c95a1e2da38f00b7f0410a62c52fa4
Moreno Ambrosin <ambrosin@google.com>
2025-03-27 12:31:05 -0700

    Delete run_go_mod_tests.sh
    PiperOrigin-RevId: 741243240
    Change-Id: I8084136793d3c95a0a72a1009b79e98f6322ee26

--- kokoro/macos_external/gomod/run_tests.sh
+++ kokoro/macos_external/gomod/run_tests.sh
@@ -25,10 +25,8 @@ fi
 source ./kokoro/testutils/install_go.sh
 echo "Using go binary from $(which go): $(go version)"
 
-readonly MODULE_URL="github.com/tink-crypto/tink-go-hcvault"
-readonly MODULE_VERSION="$(cat integration/hcvault/hcvault_client.go \
-                          | grep '// Version:' \
-                          | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"
-
-./kokoro/testutils/run_go_mod_tests.sh "${MODULE_URL}" "$(pwd)" \
-  "${MODULE_VERSION}" "main"
+(
+  set -x
+  go build -v ./...
+  go test -v ./...
+)
